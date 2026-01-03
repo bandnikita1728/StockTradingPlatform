b9c7f305361831b9f80b064314bf21a1cb4e1c72
nikita <nikitaband635@gmail.com>
2026-01-03 10:15:44 +0530

    Configured project dependencies

---
+++ frontend/src/landing_page/OpenAccount.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function OpenAccount() {
+  return (
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <h1 className="mt-5">Open a free trading account</h1>
+        <p className="text-muted">
+          Modern platforms and apps, ₹0 delivery investments, and flat ₹20 intraday and F&amp;O trades.
+        </p>
+        <div>
+          <Link to="/signup" className="btn btn-primary btn-lg px-5 mb-5">
+            Sign up free
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OpenAccount;
