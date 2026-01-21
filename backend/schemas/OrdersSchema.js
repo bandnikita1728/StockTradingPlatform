ce90cebb8378de94a75ff741624b39d96c2a6fec
nikita <nikitaband635@gmail.com>
2026-01-21 09:55:44 +0530

    Creating Schemas and Models

---
+++ backend/schemas/OrdersSchema.js
@@ -0,0 +1,13 @@
+const { Schema } = require("mongoose");
+
+const OrdersSchema = new Schema({
+  name: String,
+  qty: Number,
+  price: Number,
+  mode: String,
+  option: { type: String, default: "MIS" },
+  status: { type: String, default: "COMPLETE" },
+  date: { type: Date, default: Date.now },
+});
+
+module.exports = { OrdersSchema };
